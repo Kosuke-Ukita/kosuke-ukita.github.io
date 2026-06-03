b3e8a6b3e9c4e1f9fea5482d7ea0138c2aab363c
sha-ce <ukita.kosuke00@gmail.com>
2026-06-03 23:20:52 +0900

    update.

--- pages/[[locale]]/news/[slug].vue
+++ pages/[[locale]]/news/[slug].vue
@@ -26,7 +26,6 @@ useHead({ title: article.value.title })
       <Icon name="heroicons:arrow-left" class="w-3 h-3" />
       Back to News
     </NuxtLink>
-    <h1 class="font-mono font-semibold text-gray-900 dark:text-zinc-100 text-xl mb-8 tracking-tight">News —</h1>
 
     <article :class="['markdown', isJp ? 'show-jp' : 'show-en']">
       <ContentRenderer v-if="article" :value="article" />
