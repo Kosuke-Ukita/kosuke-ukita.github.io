046cac1e01727786e1b06e9a18cf0f6e86174648
sha-ce <ukita.kosuke00@gmail.com>
2026-06-13 14:17:40 +0800

    update.

--- pages/[[locale]]/education.vue
+++ pages/[[locale]]/education.vue
@@ -5,6 +5,8 @@ useHead({ title: 'Education' })
 <template>
   <div>
     <h1 class="font-mono font-semibold text-gray-900 dark:text-zinc-100 text-xl mb-12 tracking-tight">Education</h1>
+
+    <h2 class="section-title"></h2>
     <EducationSection />
   </div>
 </template>
